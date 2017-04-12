2d8587a7aba11a031aaccc1b408326fdf023a206
timsoto11 <timsoto11@github.com>
2017-04-11 21:52:49 -0500

    bluetooth functionality added

--- Pothole/app/src/main/java/com/example/timothy/pothole/MainActivity.java
+++ Pothole/app/src/main/java/com/example/timothy/pothole/MainActivity.java
@@ -25,6 +25,18 @@ import android.hardware.SensorEventListener;
 import android.widget.TextView;
 import android.content.Intent;
 import android.provider.Settings;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.Set;
+import java.util.ArrayList;
+import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.view.View.OnClickListener;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -33,7 +45,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager mSensorManager;
     private LocationManager mLocationManager;
     private LocationListener mLocationListener;
-
+    private Button btnPaired;
+    private ListView devicelist;
+    private BluetoothAdapter myBluetooth = null;
+    private Set pairedDevices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,19 +57,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        //fab.setOnClickListener(new View.OnClickListener() {
-        //   @Override
-        //    public void onClick(View view) {
-        //        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        //                .setAction("Action", null).show();
-        //    }
-        //});
+      //  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+      //  fab.setOnClickListener(new View.OnClickListener() {
+      //     @Override
+      //      public void onClick(View view) {
+      //          Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+       //                 .setAction("Action", null).show();
+       //     }
+       // });
 
         //initialize sensor
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+
+        btnPaired = (Button)findViewById(R.id.button);
+
+        btnPaired.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                pairedDevicesList(); //method that will be called
+            }
+        });
+
+
+
+        devicelist = (ListView)findViewById(R.id.listView);
 
         //initialize GPS
         mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
@@ -90,6 +119,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
         }
 
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
+        if(myBluetooth == null) {
+            //Show a mensag. that thedevice has no bluetooth adapter
+            Toast.makeText(getApplicationContext(), "Bluetooth Device Not Available", Toast.LENGTH_LONG).show();
+            //finish apk
+            finish();
+        }
+        else {
+            if (myBluetooth.isEnabled())
+            { }
+            else
+            {
+                //Ask to the user turn the bluetooth on
+                Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(turnBTon, 1);
+            }
+        }
+
         mLocationManager.requestLocationUpdates("gps", 1000, 0, mLocationListener);
         Pos = (TextView) findViewById(R.id.Pos);
         time = (TextView) findViewById(R.id.time);
@@ -98,7 +145,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         YMov = (TextView) findViewById(R.id.YMov);
         ZMov = (TextView) findViewById(R.id.ZMov);
         mag = (TextView) findViewById(R.id.mag);
+
     }
+
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -145,4 +194,41 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void pairedDevicesList() {
+        pairedDevices = myBluetooth.getBondedDevices();
+        ArrayList list = new ArrayList();
+
+        if (pairedDevices.size()>0)
+        {
+            Toast.makeText(getApplicationContext(), "We found devices!", Toast.LENGTH_LONG).show();
+            for(Object bt : pairedDevices)
+            {
+               list.add(((BluetoothDevice)bt).getName() + "\n" + ((BluetoothDevice)bt).getAddress()); //Get the device's name and the address
+            }
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "No Paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
+        }
+
+        final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+        devicelist.setAdapter(adapter);
+        devicelist.setOnItemClickListener(myListClickListener); //Method called when the device from the list is clicked
+
+    }
+
+    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener() {
+        public void onItemClick (AdapterView av, View v, int arg2, long arg3)
+        {
+            // Get the device MAC address, the last 17 chars in the View
+            String info = ((TextView) v).getText().toString();
+            String address = info.substring(info.length() - 17);
+            // Make an intent to start next activity.
+            //Intent i = new Intent(DeviceList.this, ledControl.class);
+            //Change the activity.
+            //i.putExtra(EXTRA_ADDRESS, address); //this will be received at ledControl (class) Activity
+           // startActivity(i);
+        }
+    };
 }
